c4b3efb039f305b72ca7b3a5517eb4e0b589d831
Tormod Erevik Lea <tormodlea@gmail.com>
2016-04-20 20:02:50 +0200

    consts: export two version constants and remove unused ones

--- consts.go
+++ consts.go
@@ -1,15 +1,19 @@
 package fit
 
-const (
-	protocolVersion           byte = ((protocolMajorVersion << protocolVersionMajorShift) + protocolMinorVersion)
-	protocolMajorVersion           = 1
-	protocolMinorVersion           = 0
-	protocolVersionMajorShift      = 4
-	protocolVersionMajorMask       = (0x0F << protocolVersionMajorShift)
+// ProfileVersion is the current supported profile version of the FIT SDK.
+const ProfileVersion uint16 = ((profileMajorVersion * 100) + profileMinorVersion)
 
-	profileVersion      uint16 = ((profileMajorVersion * 100) + profileMinorVersion)
-	profileMajorVersion        = 16
-	profileMinorVersion        = 20
+// ProtocolVersion is the current supported FIT protocol version.
+const ProtocolVersion byte = ((protocolMajorVersion << protocolVersionMajorShift) + protocolMinorVersion)
+
+const (
+	profileMajorVersion = 16
+	profileMinorVersion = 20
+
+	protocolMajorVersion      = 1
+	protocolMinorVersion      = 0
+	protocolVersionMajorShift = 4
+	protocolVersionMajorMask  = (0x0F << protocolVersionMajorShift)
 
 	headerTypeMask             byte = 0xF0
 	compressedHeaderMask            = 0x80
@@ -19,14 +23,12 @@ const (
 	mesgDefinitionMask byte = 0x40
 	mesgHeaderMask          = 0x00
 	localMesgNumMask        = 0x0F
-	maxLocalMesgs           = localMesgNumMask + 1
 
-	mesgDefinitionReserved byte = 0x00
-	littleEndian                = 0x00
-	bigEndian                   = 0x01
+	maxLocalMesgs = localMesgNumMask + 1
+	maxFieldSize  = 255
 
-	maxMesgSize  byte = 255
-	maxFieldSize      = 255
+	littleEndian = 0x00
+	bigEndian    = 0x01
 
 	bytesForCRC          = 2
 	headerSizeCRC   byte = 14
@@ -34,10 +36,18 @@ const (
 
 	fitDataTypeString = ".FIT"
 
-	fieldNumInvalid   byte = 255
-	fieldNumTimeStamp      = 253
+	fieldNumTimeStamp = 253
+)
 
+// Constants taken from offical SDK code but unused.
+// TODO(tormoder): Check if they can be used somewhere or remove.
+/*
+const (
+	mesgDefinitionReserved byte = 0x00
+	maxMesgSize   byte = 255
+	fieldNumInvalid byte = 255
 	subfieldIndexMainField      uint16 = subfieldIndexActiveSubfield + 1
 	subfieldIndexActiveSubfield        = 0xFFFE
 	subfieldNameMainField       string = ""
 )
+*/

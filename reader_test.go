6d68ebe854fd72839e83dd89793fe7fd8c4f87b8
Tormod Erevik Lea <tormodlea@gmail.com>
2017-09-18 15:02:47 +0200

    reader_test: update Go version (1.9) used for golden decode tests

--- reader_test.go
+++ reader_test.go
@@ -62,7 +62,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestDecode(t *testing.T) {
-	const goMajorVersionForDecodeGolden = "go1.8"
+	const goMajorVersionForDecodeGolden = "go1.9"
 	testDecodeGolden := true
 	goVersion := runtime.Version()
 	goVersionOK := strings.HasPrefix(goVersion, goMajorVersionForDecodeGolden)

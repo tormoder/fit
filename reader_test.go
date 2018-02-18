0085f3407a94289674aeed7ac8c57ec312e39486
Tormod Erevik Lea <tormodlea@gmail.com>
2018-02-18 12:20:31 +0100

    reader_test: bump Go version for decode tests to 1.10

--- reader_test.go
+++ reader_test.go
@@ -62,7 +62,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestDecode(t *testing.T) {
-	const goMajorVersionForDecodeGolden = "go1.9"
+	const goMajorVersionForDecodeGolden = "go1.10"
 	testDecodeGolden := true
 	goVersion := runtime.Version()
 	goVersionOK := strings.HasPrefix(goVersion, goMajorVersionForDecodeGolden)

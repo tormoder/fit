54f9f2c02da60ce018435d947d4680c54b63a086
Brian Starkey <stark3y@gmail.com>
2019-04-28 14:14:15 +0100

    generator_test: Calculate golden fingerprints at runtime
    To make the test work across gofmt changes, calculate the golden
    fingerprint at runtime, by running the golden data through
    go/format.Source()
    
    This ensures that the go code is equivalent, but doesn't care if the
    golden output was generated with a different version of gofmt.

--- cmd/fitgen/internal/profile/generator_test.go
+++ cmd/fitgen/internal/profile/generator_test.go
@@ -1,13 +1,16 @@
 package profile_test
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
+	"go/format"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/cespare/xxhash"
@@ -66,6 +69,108 @@ func writeProfileToFile(p *profile.Profile, path string) error {
 	return ioutil.WriteFile(path, buf.Bytes(), 0644)
 }
 
+func scanLinesPreserveEOL(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, data[0 : i+1], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+func readGoldenProfile(path string) (*profile.Profile, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }() // Sigh. To keep errcheck happy
+
+	p := &profile.Profile{}
+	headings := []string{
+		"// TYPES",
+		"// MESSAGES",
+		"// PROFILE",
+		"// STRINGER TYPE INPUT",
+		"// MESSAGE NUMS WITHOUT MESSAGE",
+	}
+	i := 0
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(scanLinesPreserveEOL)
+
+	scanner.Scan()
+	err = scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	if !strings.HasPrefix(scanner.Text(), headings[i]) {
+		return nil, fmt.Errorf("first line should be '%s'", headings[i])
+	}
+
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), headings[i+1]) {
+			i++
+			break
+		}
+		p.TypesSource = append(p.TypesSource, scanner.Bytes()...)
+	}
+
+	// Format
+	p.TypesSource, err = format.Source(p.TypesSource)
+	if err != nil {
+		return nil, err
+	}
+
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), headings[i+1]) {
+			i++
+			break
+		}
+		p.MessagesSource = append(p.MessagesSource, scanner.Bytes()...)
+	}
+
+	// Format
+	p.MessagesSource, err = format.Source(p.MessagesSource)
+	if err != nil {
+		return nil, err
+	}
+
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), headings[i+1]) {
+			i++
+			break
+		}
+		p.ProfileSource = append(p.ProfileSource, scanner.Bytes()...)
+	}
+
+	// Format
+	p.ProfileSource, err = format.Source(p.ProfileSource)
+	if err != nil {
+		return nil, err
+	}
+
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), headings[i+1]) {
+			break
+		}
+		p.StringerInput += strings.TrimSpace(scanner.Text())
+	}
+
+	for scanner.Scan() {
+		p.MesgNumsWithoutMessage = append(p.MesgNumsWithoutMessage, strings.TrimSpace(scanner.Text()))
+	}
+
+	return p, scanner.Err()
+}
+
 func profileFingerprint(p *profile.Profile) uint64 {
 	h := xxhash.New()
 	_ = writeProfile(p, h)
@@ -73,15 +178,14 @@ func profileFingerprint(p *profile.Profile) uint64 {
 }
 
 type sdk struct {
-	majVer, minVer    int
-	goldenFingerprint uint64
+	majVer, minVer int
 }
 
 var sdks = []sdk{
-	{16, 20, 9140511954122483521},
-	{20, 14, 1110026377375606382},
-	{20, 27, 17729391755185608044},
-	{20, 43, 2113486054852011886},
+	{16, 20},
+	{20, 14},
+	{20, 27},
+	{20, 43},
 }
 
 func TestMain(m *testing.M) {
@@ -112,10 +216,19 @@ func TestGenerator(t *testing.T) {
 				t.Fatal(err)
 			}
 			gotFP := profileFingerprint(p)
-			if gotFP == sdk.goldenFingerprint {
+
+			// Read in the golden profile, format it and fingerprint it
+			// This makes the test robust against gofmt changes
+			goldenProfile, err := readGoldenProfile(path + ".golden")
+			if err != nil {
+				t.Fatal(err)
+			}
+			goldenFingerprint := profileFingerprint(goldenProfile)
+
+			if gotFP == goldenFingerprint {
 				return
 			}
-			t.Errorf("profile fingerprint differs: got: %d, want %d", gotFP, sdk.goldenFingerprint)
+			t.Errorf("profile fingerprint differs: got: %d, want %d", gotFP, goldenFingerprint)
 			if !*update {
 				path = path + currentSuffix
 			} else {

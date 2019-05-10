0a1f13cd4e62cee07061a920ad669fbe0ac195cf
Tormod Erevik Lea <tormodlea@gmail.com>
2019-05-10 21:29:21 +0200

    fuzz: update fuzz function to also perform encoding

--- fuzz.go
+++ fuzz.go
@@ -2,11 +2,25 @@
 
 package fit
 
-import "bytes"
+import (
+	"bytes"
+	"encoding/binary"
+)
 
 func Fuzz(data []byte) int {
-	if _, err := Decode(bytes.NewReader(data)); err != nil {
+	fitFile, err := Decode(bytes.NewReader(data))
+	if err != nil {
+		if fitFile != nil {
+			panic("fitFile != nil on error")
+		}
 		return 0
 	}
+
+	var w bytes.Buffer
+	err = Encode(&w, fitFile, binary.LittleEndian)
+	if err != nil {
+		panic(err)
+	}
+
 	return 1
 }
